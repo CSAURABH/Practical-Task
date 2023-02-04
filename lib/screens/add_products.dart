4f0b9953b172e4ab3d97bcf3d752ad87d3b70e06
1919Saurabh <chacheresaurabh19@gmail.com>
2023-02-05 02:13:12 +0530

    commit changes

--- lib/screens/add_products.dart
+++ lib/screens/add_products.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,6 +10,39 @@ class AddProducts extends StatefulWidget {
 }
 
 class _AddProductsState extends State<AddProducts> {
+  TextEditingController productName = TextEditingController();
+  TextEditingController categoryName = TextEditingController();
+  TextEditingController companyName = TextEditingController();
+  TextEditingController description = TextEditingController();
+  TextEditingController price = TextEditingController();
+  TextEditingController quantity = TextEditingController();
+
+  addProduct() async {
+    String pName = productName.text.trim();
+    // String catName = categoryName.text.trim();
+    // String cpyName = companyName.text.trim();
+    String descName = description.text.trim();
+    String priceName = price.text.trim();
+    String qtyName = quantity.text.trim();
+
+    if (productName.text.trim().isEmpty ||
+        description.text.trim().isEmpty ||
+        price.text.trim().isEmpty ||
+        quantity.text.trim().isEmpty) {
+    } else {
+      CollectionReference collectionReference =
+          FirebaseFirestore.instance.collection('Product');
+      Map<String, dynamic> product = {
+        'product-name': pName,
+        'description-name': descName,
+        'price-name': priceName,
+        'qty-name': qtyName,
+      };
+      companyName.clear();
+      return collectionReference.add(product);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +65,7 @@ class _AddProductsState extends State<AddProducts> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextFormField(
+                  controller: productName,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(5.0),
@@ -76,6 +111,7 @@ class _AddProductsState extends State<AddProducts> {
                 ),
                 SizedBox(height: 16.h),
                 TextFormField(
+                  controller: description,
                   minLines: 3,
                   maxLines: 5,
                   keyboardType: TextInputType.multiline,
@@ -91,6 +127,7 @@ class _AddProductsState extends State<AddProducts> {
                 ),
                 SizedBox(height: 16.h),
                 TextFormField(
+                  controller: price,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(5.0),
@@ -100,6 +137,7 @@ class _AddProductsState extends State<AddProducts> {
                 ),
                 SizedBox(height: 16.h),
                 TextFormField(
+                  controller: quantity,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(5.0),
@@ -165,7 +203,9 @@ class _AddProductsState extends State<AddProducts> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xff6D7072),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      addProduct();
+                    },
                     child: Text(
                       'Save',
                       style: TextStyle(fontSize: 18.sp),
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:practical_task/screens/add_products.dart';
+import 'package:practical_task/screens/detail_screen.dart';
 
 class ProductScreen extends StatelessWidget {
   const ProductScreen({super.key});
@@ -33,90 +35,119 @@ class ProductScreen extends StatelessWidget {
             SizedBox(width: 16.23.sp),
           ],
         ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Card(
-              margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 13.h),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 13.w, vertical: 16.h),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 77.h,
-                          width: 77.w,
-                          child: const Card(
-                            color: Color(0xff6D7072),
-                            child: Center(
-                              child: Text('Image'),
-                            ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("Product").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> name = snapshot.data!.docs[index].data();
+                DocumentSnapshot documentSnapshot = snapshot.data!.docs[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const DetailScreen(),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 13.h),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 13.w, vertical: 16.h),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 77.h,
+                                width: 77.w,
+                                child: const Card(
+                                  color: Color(0xff6D7072),
+                                  child: Center(
+                                    child: Text('Image'),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 9.sp,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  SizedBox(height: 10.h),
+                                  Text(
+                                    name['product-name'],
+                                    style: TextStyle(
+                                        fontSize: 14.sp,
+                                        color: const Color(0xff6D7072)),
+                                  ),
+                                  Text(
+                                    'Computer & Accessories',
+                                    style: TextStyle(
+                                        fontSize: 9.sp,
+                                        color: const Color(0xff6D7072)),
+                                  ),
+                                  Text(
+                                    'Qty : ${name['qty-name']}',
+                                    style: TextStyle(
+                                        fontSize: 10.sp,
+                                        color: const Color(0xff6D7072)),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
-                        ),
-                        SizedBox(
-                          width: 9.sp,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SizedBox(height: 10.h),
-                            Text(
-                              'LG LCD Monitor',
-                              style: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: const Color(0xff6D7072)),
-                            ),
-                            Text(
-                              'Computer & Accessories',
-                              style: TextStyle(
-                                  fontSize: 9.sp,
-                                  color: const Color(0xff6D7072)),
-                            ),
-                            Text(
-                              'Qty : 05',
-                              style: TextStyle(
-                                  fontSize: 10.sp,
-                                  color: const Color(0xff6D7072)),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(
+                                width: 77.w,
+                                height: 28.h,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xff6D7072),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text('Edit'),
+                                ),
+                              ),
+                              SizedBox(height: 19.h),
+                              SizedBox(
+                                width: 77.w,
+                                height: 28.h,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xff6D7072),
+                                  ),
+                                  onPressed: () {
+                                    FirebaseFirestore.instance
+                                        .collection('Product')
+                                        .doc(documentSnapshot.id)
+                                        .delete();
+                                  },
+                                  child: const Text('Delete'),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        SizedBox(
-                          width: 77.w,
-                          height: 28.h,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xff6D7072),
-                            ),
-                            onPressed: () {},
-                            child: const Text('Edit'),
-                          ),
-                        ),
-                        SizedBox(height: 19.h),
-                        SizedBox(
-                          width: 77.w,
-                          height: 28.h,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xff6D7072),
-                            ),
-                            onPressed: () {},
-                            child: const Text('Delete'),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             );
           },
         ));
